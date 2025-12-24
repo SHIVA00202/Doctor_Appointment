19814f6c5762f9fbb2c0b2e481bbc775461a38d3
SHIVA00202 <shivayadav1238800@gmail.com>
2025-12-25 03:35:52 +0530

    Implementing Dashboard Page For Doctor

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -11,6 +11,7 @@ const DoctorContextProvider = (props) => {
     localStorage.getItem("dToken") ? localStorage.getItem("dToken") : ""
   );
   const [appointments, setAppointments] = useState([]);
+    const [dashData, setDashData] = useState(false);
 
   
   
@@ -73,6 +74,24 @@ const DoctorContextProvider = (props) => {
     }
   };
 
+   const getDashData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/doctor/dashboard", {
+        headers: { dToken },
+      });
+
+      if (data.success) {
+        setDashData(data.dashData);
+        console.log(data.dashData);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
   
   const value = {
     dToken,
@@ -83,6 +102,9 @@ const DoctorContextProvider = (props) => {
     completeAppointment,
     cancelAppointment,    
     getAppointments,
+    dashData,
+    getDashData,
+setDashData
   };
 
   return (
